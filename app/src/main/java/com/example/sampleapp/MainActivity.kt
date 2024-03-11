8baf2d7c64083a31916befd21d033cf30f4f9362
subhadipverloop <159517480+subhadipverloop@users.noreply.github.com>
2024-03-11 16:30:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.sampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import io.verloop.sdk.Verloop
+import io.verloop.sdk.VerloopConfig
+import java.util.UUID
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        capturing click event
+        val click = findViewById<Button>(R.id.click_verloop);
+
+        click.setOnClickListener {
+            Toast.makeText(this, "Verloop Button Clicked", Toast.LENGTH_SHORT).show()
+
+            val uid = UUID.randomUUID().toString()
+
+            var customFields: ArrayList<VerloopConfig.CustomField> = ArrayList()
+            customFields.add(
+                VerloopConfig.CustomField(
+                    "name",
+                    "androidUser",
+                    scope = VerloopConfig.Scope.ROOM
+                )
+            )
+            val config = VerloopConfig.Builder()
+                .clientId("subhadipmondal")
+                .userId(uid)
+                .userName("android User")
+                .userEmail("androiduser@gmail.com")
+                .closeExistingChat(false)
+                .fields(customFields)
+                .build()
+            val verloop = Verloop(this,config)
+            verloop.showChat()
+        }
+    }
+}
