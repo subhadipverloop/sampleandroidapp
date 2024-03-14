99176ff7c00d3ebc08af8c2ff1b905dcfde194d1
subhadipverloop <159517480+subhadipverloop@users.noreply.github.com>
2024-03-14 14:07:36 +0530

    done

--- app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -12,13 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val uid = UUID.randomUUID().toString()
 
 //        capturing click event
         val click = findViewById<Button>(R.id.click_verloop);
 
         click.setOnClickListener {
             Toast.makeText(this, "Verloop Button Clicked", Toast.LENGTH_SHORT).show()
-
+//            var uid = uid
             val uid = UUID.randomUUID().toString()
 
             var customFields: ArrayList<VerloopConfig.CustomField> = ArrayList()
